b1ede37b5aa34144aa478836fd7f1c276a31cf89
Dev Aryan Dogra <devdogra1@gmail.com>
2023-11-03 17:45:08 +0530

    add lec3 (g24)

---
+++ g24/lec3/server.js
@@ -0,0 +1,24 @@
+const http = require('http'); 
+
+const users = [
+    { name: "Dev", age: 22},
+    { name: "Hitesh", age: 23}
+]
+const products = [
+    { name: "Eraser", price: 10 }, 
+    { name: "Pencil", price: 5 }
+]
+
+const server = http.createServer((req, res) => {
+    console.log(req.url); 
+    res.setHeader("Content-Type", "application/json");
+    if (req.url == '/users') {
+        res.end(JSON.stringify(users));
+    } else if (req.url == '/products') {
+        res.end(JSON.stringify(products));
+    } else {
+        res.end("Error 404"); 
+    }
+})
+
+server.listen(3000, () => console.log("Server listenting at http://localhost:3000"))
