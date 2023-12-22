ef6590fa669ba815ee343789a376cd0fc0da18d5
Dev Aryan Dogra <devdogra1@gmail.com>
2023-12-22 17:23:42 +0530

    Add lec6 ppt link

--- g24/lec6/server.js
+++ g24/lec6/server.js
@@ -3,17 +3,33 @@ const cookieParser = require("cookie-parser");
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('./models/User.js');
-
+const session = require('express-session'); 
 mongoose.connect("mongodb://127.0.0.1:27017/AuthG24")
-
 const app = express(); 
 
+app.use(session({
+    secret: 'this is my secret'
+}))
 app.use(cookieParser());
 app.use(express.json()); 
 app.use(express.urlencoded()); 
 
 app.set('view engine', 'hbs'); 
 app.set('views', './views')
+
+function isLoggedIn(req, res, next) {
+    if (req.session.loggedIn) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+
+app.get('/protected', isLoggedIn, (req, res) => {
+    res.send("Welcome to protected"); 
+})
+// app.get('/profile', isLoggedIn, (req, res) => { ... })
 
 app.get('/shop', (req, res) => {
     const visited = req.cookies.visited; 
@@ -42,25 +58,26 @@ app.post('/register', async (req, res) => {
     await newUser.save();
     res.send("Registration succesful"); 
 })
+
 app.post('/login', async (req, res) => {
     const user = req.body; 
     if (!user.username || !user.password) {
         res.send("Username and password are required"); 
         return; 
     }
-
     const account =  await User.findOne({
         username: user.username
     })
 
     if (account.password == user.password) {
+        req.session.loggedInUser = account;
+        req.session.loggedIn = true;
         res.send("Login succesful");
     } else {
         res.send("Incorrect password");
     }
-    // const account = await User.findOne().where('username').equals(user.username);
-    // res.send("Invalid login"); 
 })
+
 app.get('/register', (req, res) => {
     res.render('register'); 
 })
